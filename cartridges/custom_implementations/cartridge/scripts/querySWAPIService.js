7725f1b4e01b20c48d3c9d6cc6386258afb387da
giulio <giulio.brc@gmail.com>
2022-12-16 12:08:46 +0100

    PIPPO-100 wishlist implemented wishlist cartridge

--- cartridges/custom_implementations/cartridge/scripts/querySWAPIService.js
+++
@@ -1,9 +0,0 @@
-var SWAPIService = require('~/cartridge/scripts/services/swapi');
-
-function callSWAPI(resource) {
-  var result = SWAPIService.call(resource);
-
-  return result;
-}
-
-module.export = callSWAPI;
